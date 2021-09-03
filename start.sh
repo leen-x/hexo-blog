7bb61e18fdac2820c6f3be74dd2fb1c7547b2973
leen-x <linsongxiong2015@163.com>
2021-09-03 10:57:57 +0800

    [INI]

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 rm -rf /data/docker/nginx-docker/html
-cp /data/workspace/hexo-blog/public /data/docker/nginx-docker/html
+cp -r /data/workspace/hexo-blog/public /data/docker/nginx-docker/html
 docker restart my_nginx
